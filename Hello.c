270d7c64748b576403802620fa8d6f52eb19b20a
jslee000 <candle3@hanmail.net>
2023-01-22 14:39:25 +0900

    Modify

--- Hello.c
+++ Hello.c
@@ -6,8 +6,9 @@ void main(void)
     putchar(65);//숫자 : A 
     putchar(0101);//8진수 : K 
     putchar(0x41);//16진수 : K 
+    putchar(0x42);//16진수 : L 
     putchar('\n');//한줄내림 
-
+    
     printf("%c %d %o %x \n"
         , 'A'    //문자 : %c 
         , 'A'    //10진수 : %d 
