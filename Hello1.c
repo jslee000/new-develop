e469b44c821f335eff8bcbf560f4d2bf02c48b23
jslee000 <candle3@hanmail.net>
2023-01-23 21:15:06 +0900

    all File Push

--- Hello1.c
+++ Hello1.c
@@ -1,3 +1,6 @@
+#-- Hello1.c --#
+
+
 #include <stdio.h> 
 
 void main(void)
@@ -6,8 +9,10 @@ void main(void)
     putchar(65);//숫자 : A 
     putchar(0101);//8진수 : K 
     putchar(0x41);//16진수 : K 
+    putchar(0x42);//16진수 : L M
+    putchar(0x42);//16진수 : L M1
     putchar('\n');//한줄내림 
-
+    
     printf("%c %d %o %x \n"
         , 'A'    //문자 : %c 
         , 'A'    //10진수 : %d 
---
+++ Hello3.c
@@ -0,0 +1,19 @@
+#-- Hello3.c --#
+
+#include <stdio.h> 
+
+void main(void)
+{
+    putchar('A');//문자 : A 
+    putchar(65);//숫자 : A 
+    putchar(0101);//8진수 : K 
+    putchar(0x41);//16진수 : K 
+    putchar('\n');//한줄내림 
+
+    printf("%c %d %o %x \n"
+        , 'A'    //문자 : %c 
+        , 'A'    //10진수 : %d 
+        , 'A'    //8진수 : %o 
+        , 'A'    //16진수 : %x 
+    );
+}
